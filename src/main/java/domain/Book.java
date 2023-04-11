c655574eb5ac0ba893de33ba4dbda82754fe0388
Savannah-sun <yiqian.sun@tufts.edu>
2023-04-11 11:09:16 -0400

    two data entities

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,15 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+}
